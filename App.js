b5ff2a8a2dedd88b1bf4796239276dce66baacd6
gembkkim <gembkkim@gmail.com>
2025-11-02 03:51:23 +0900

    test

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // import { PaperProvider } from "react-native-paper";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
+// import { NavigationContainer } from "@react-navigation/native";
 // 응용 화면들...
 import LoginScreen from "./screens/LoginScreen";
 import BackendUsersScreen from "./screens/BackendUsersScreen";
@@ -15,26 +14,6 @@ import TestScreen from "./screens/TestScreen";
 import StyleScreen from "./screens/StyleScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserInfoScreen from "./screens/UserInfoScreen";
-import RNCameraScreen from "./screens/RNCameraScreen";
-
-// 예시 화면 컴포넌트
-function HomeScreen() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View>
-      <Text>Settings</Text>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -43,16 +22,7 @@ export default function App() {
     // <PaperProvider theme={theme}>
     <Provider store={store}>
       <NavigationContainer>
-        {/* <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator> */}
         <Stack.Navigator initialRouteName="UserList">
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="RNCamera"
-            component={RNCameraScreen}
-          />
           <Stack.Screen
             options={{ headerShown: false }}
             name="UserInfo"
@@ -99,12 +69,3 @@ export default function App() {
     // </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
